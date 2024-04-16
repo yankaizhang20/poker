d681d162cb78582f5f7ed4799f8562006edc9e98
yankaizhang20 <yankaizhang2020@163.com>
2024-04-16 23:23:52 +0800

    使 thread-manage 依赖 thread-lock-hierarchical

---
+++ thread/lock/hierarchical/src/hierarchical_mutex.cpp
@@ -0,0 +1,13 @@
+//
+// Created by zyk on 24-4-16.
+//
+
+#include "hierarchical_mutex.h"
+
+
+namespace poker::thread::lock
+{
+    void TestHierarchical()
+    {
+    }
+}   // namespace poker::thread::lock
--- thread/manage/src/manage.cpp
+++ thread/manage/src/manage.cpp
@@ -2,11 +2,13 @@
 // Created by zyk on 24-4-15.
 //
 #include <iostream>
+#include "hierarchical_mutex.h"
 
 namespace poker::thread::manage
 {
     void Test()
     {
+        lock::TestHierarchical();
         std::cout << "just for test" << std::endl;
     }
 }   // namespace poker::thread::manage
