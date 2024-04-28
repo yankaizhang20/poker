51c6f0da9e832c7ec3b90fa786660d2d3a649dfe
yankaizhang20 <yankaizhang2020@163.com>
2024-04-28 20:29:53 +0800

    线程安全队列-第一版

--- thread/container/queue/include/poker/thread/queue.h
+++ thread/container/queue/include/poker/thread/queue.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <condition_variable>
 #include <memory>
 #include <queue>
 #include <shared_mutex>
@@ -15,21 +16,86 @@ namespace poker::thread
     class Queue
     {
     public:
-        void push(const T &value);
+        void push(const T &value)
+        {
+            {
+                std::lock_guard guard(m_);
+
+                queue_.push(value);
+            }
+            con_.notify_one();
+        }
 
         template < class... Args >
-        void emplace(Args &&...args);
+        void emplace(Args &&...args)
+        {
+            {
+                std::lock_guard guard(m_);
 
-        bool try_pop(T &value);
+                queue_.emplace(std::forward< Args >(args)...);
+            }
+            con_.notify_one();
+        }
 
-        std::shared_ptr< T > try_pop();
+        bool try_pop(T &value)
+        {
+            std::lock_guard guard(m_);
 
-        void wait_and_pop(T &value);
+            if (queue_.empty())
+            {
+                return false;
+            }
 
-        std::shared_ptr< T > wait_and_pop();
+            value = queue_.front();
+
+            queue_.pop();
+
+            return true;
+        }
+
+        std::shared_ptr< T > try_pop()
+        {
+            std::lock_guard guard(m_);
+
+            if (queue_.empty())
+            {
+                return false;
+            }
+
+            auto p_top = std::make_shared< T >(queue_.front());
+
+            queue_.pop();
+
+            return p_top;
+        }
+
+        void wait_and_pop(T &value)
+        {
+            std::unique_lock guard(m_);
+
+            con_.wait(guard, [ this ]() { return !queue_.empty(); });
+
+            value = queue_.front();
+
+            queue_.pop();
+        }
+
+        std::shared_ptr< T > wait_and_pop()
+        {
+            std::unique_lock guard(m_);
+
+            con_.wait(guard, [ this ]() { return !queue_.empty(); });
+
+            auto p_top = std::make_shared< T >(queue_.front());
+
+            queue_.pop();
+
+            return p_top;
+        }
 
     private:
-        std::shared_mutex m_;
+        std::shared_mutex       m_;
+        std::condition_variable con_;
 
         std::queue< T > queue_;
     };
