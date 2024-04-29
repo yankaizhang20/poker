0ffdd1b5c362da375b5aaabdc70650754c5199a9
yankaizhang20 <yankaizhang2020@163.com>
2024-04-29 12:17:49 +0800

    线程安全队列-细粒度ing

--- thread/container/queue/include/poker/thread/queue.h
+++ thread/container/queue/include/poker/thread/queue.h
@@ -15,7 +15,7 @@ namespace poker::thread
     class Queue
     {
     public:
-        Queue() : head_(new Node()), tail_(head_)
+        Queue() : p_head_(new Node()), p_tail_(p_head_.get())
         {
         }
         Queue(const Queue &other)
@@ -25,16 +25,24 @@ namespace poker::thread
     public:
         void push(const T &value)
         {
-            auto p_dummy_node = new Node();
+            // 提前构造新数据
+            auto p_data = std::make_shared< T >(value);
 
-            std::lock_guard guard(t_m_);
+            // 提前构造傀儡节点
+            std::unique_lock< Node > p_dummy_node(new Node());
 
-            // 在原有尾部构造新数据
-            tail_->p_data = std::make_shared< T >(value);
-            tail_->next   = p_dummy_node;
+            {
+                std::lock_guard guard(t_m_);
 
-            // 使 tail 指向新的傀儡节点
-            tail_ = p_dummy_node;
+                // 在原有尾部构造新数据
+                p_tail_->p_data = std::move(p_data);
+                p_tail_->next   = std::move(p_dummy_node);
+
+                // 使 tail 指向新的傀儡节点
+                p_tail_ = p_dummy_node.get();
+            }
+
+            con_.notify_one();
         }
 
         template < class... Args >
@@ -44,43 +52,88 @@ namespace poker::thread
 
         bool try_pop(T &value)
         {
-            std::lock_guard guard(h_m_);
+            auto p_head = try_pop_head();
 
-            // 若为初始状态，则返回 false
+            if (!p_head)
+            {
+                return false;
+            }
 
-            auto old_head = head_;
-            value         = *old_head->p_data;
-
-            head_ = old_head->next;
-
-            delete old_head;
+            value = *p_head->p_data;
+            return true;
         }
 
         std::shared_ptr< T > try_pop()
         {
+            auto p_head = try_pop_head();
+
+            return p_head ? p_head->p_data : nullptr;
         }
 
         void wait_and_pop(T &value)
         {
+            auto p_head = wait_and_pop();
+
+            value = *p_head->p_data;
         }
 
         std::shared_ptr< T > wait_and_pop()
         {
+            auto p_head = wait_and_pop();
+
+            return p_head->p_data;
         }
 
     private:
         struct Node
         {
-            std::shared_ptr< T > p_data = nullptr;
-            Node                *next   = nullptr;
+            std::shared_ptr< T >     p_data = nullptr;
+            std::unique_lock< Node > next   = nullptr;
         };
+
+        Node *get_tail()
+        {
+            std::lock_guard guard(t_m_);
+
+            return p_tail_;
+        }
+
+        std::unique_lock< Node > pop_head()
+        {
+            auto old_head = std::move(p_head_);
+
+            p_head_ = std::move(old_head->next);
+
+            return old_head;
+        }
+
+        std::unique_lock< Node > try_pop_head()
+        {
+            std::lock_guard guard(h_m_);
+
+            // 若为初始状态，则返回 false
+            if (p_head_.get() == get_tail())
+            {
+                return nullptr;
+            }
+
+            return pop_head();
+        }
+
+        std::unique_lock< Node > wait_pop_head()
+        {
+            std::unique_lock guard(h_m_);
+
+            con_.wait(guard, [ this ]() { return p_head_.get() != get_tail(); });
+
+            return pop_head();
+        }
 
     private:
         std::mutex              h_m_, t_m_;
         std::condition_variable con_;
 
-        bool is_init = true;
-
-        Node *head_, *tail_;
+        std::unique_lock< Node > p_head_;
+        Node                    *p_tail_;
     };
 }   // namespace poker::thread
